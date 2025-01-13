6f44f6854e47ad1d583b1f282625f24b034109dc
atharva624 <atharva.hambarde@cctech.co.in>
2025-01-13 11:02:25 +0530

    Dropdown_FilterNode

---
+++ FilterNode/DropdownFilterNodeView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using FilterNode;
+using System.Windows.Controls;
+
+namespace CustomDynamoNodes
+{
+    public partial class DropdownFilterNodeView : UserControl
+    {
+        private DropdownFilterNode _node;
+
+        public DropdownFilterNodeView(DropdownFilterNode node)
+        {
+            InitializeComponent();
+            _node = node;
+
+            // Bind ComboBox items to node items
+            DropdownComboBox.ItemsSource = _node.Items;
+            DropdownComboBox.SelectedItem = _node.SelectedItem;
+        }
+
+        private void DropdownComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DropdownComboBox.SelectedItem != null)
+            {
+                _node.SelectedItem = DropdownComboBox.SelectedItem.ToString();
+            }
+        }
+    }
+}
---
+++ FilterNode/DropdownFilterNodeViewCustomization.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dynamo.Graph.Nodes;
+using Dynamo.UI.Commands;
+using Newtonsoft.Json;
+using ProtoCore.AST.AssociativeAST;
+
+namespace CustomDynamoNodes
+{
+    [NodeName("Dropdown Filter Node")]
+    [NodeCategory("Custom.Nodes")]
+    [NodeDescription("Filters a string from a list of strings based on a dropdown selection.")]
+    [OutPortNames("Result")]
+    [OutPortDescriptions("The filtered string or empty if not found.")]
+    [OutPortTypes("string")]
+    public class DropdownFilterNode : NodeModel
+    {
+        private string _selectedItem;
+        private List<string> _items;
+
+        [JsonConstructor]
+        public DropdownFilterNode()
+        {
+            RegisterAllPorts();
+            Items = new List<string>();
+            SelectedItem = string.Empty;
+        }
+
+        public List<string> Items
+        {
+            get => _items;
+            set
+            {
+                _items = value;
+                RaisePropertyChanged(nameof(Items));
+            }
+        }
+
+        public string SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                _selectedItem = value;
+                RaisePropertyChanged(nameof(SelectedItem));
+                OnNodeModified();
+            }
+        }
+
+        public DelegateCommand UpdateItemsCommand => new DelegateCommand(UpdateItems);
+
+        private void UpdateItems(object obj)
+        {
+            // Populate dropdown with default items
+            Items = new List<string> { "Option1", "Option2", "Option3" };
+        }
+
+        public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+        {
+            if (inputAstNodes == null || inputAstNodes.Count < 1)
+                return new[] { AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), AstFactory.BuildNullNode()) };
+
+            var selectedAst = AstFactory.BuildStringNode(SelectedItem);
+            var inputList = inputAstNodes[0];
+            var resultCondition = AstFactory.BuildFunctionCall(
+                new Func<string, List<string>, string>((s, list) => list.Contains(s) ? s : string.Empty),
+                new List<AssociativeNode> { selectedAst, inputList });
+
+            return new[]
+            {
+                AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), resultCondition)
+            };
+        }
+    }
+}
